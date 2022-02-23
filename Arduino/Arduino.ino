a2c4be2a6dac76f5f3ee0cafc6f3ff455effdc8d
LorenzoMan <56729229+LorenzoMan@users.noreply.github.com>
2022-02-23 16:28:31 +0100

    Add comments

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -31,71 +31,62 @@ float mem = -500.0;
 static char result[6];
 
 void loop() {
-  spe = -1;
   
   delay(100);
   while (Serial.available() > 0)
  {
-   //Create a place to hold the incoming message
+   //Create a string to hold the incoming message
    static char message[MAX_MESSAGE_LENGTH];
    static unsigned int message_pos = 0;
 
    //Read the next available byte in the serial receive buffer
    char inByte = Serial.read();
-   //delay(2);
-   //Message coming in (check not terminating character) and guard for over message size
+   //Message coming in (check terminating character) and guard for over message size
    if ( inByte != ']')
    {
-     message[message_pos] = inByte; //Je mets tous les caractères dans une string du type "["throttle", 100]"
+     message[message_pos] = inByte; //Put the message in the string like => "["throttle", 100]"
      message_pos++;
    }
-   else //le message de la socket est terminé je rentre dans le else
+   else //end of message
    {
-     //Add null character to string
-     message[message_pos] = '\0';
-     for (i = 0; message[i]!= ','; i++); //j'avance jusqu'a la data de type int correspond à la valeur de l'accel ou le frein
+     message[message_pos] = '\0'; // Add null character to string
+     for (i = 0; message[i!= ','; i++); // Go to the value of the incoming data
      i ++;
      for (j = 0; message[i]!= '\0'; i++) {
-        result[j] = message[i]; //Je mets la valeur dans une string
+        result[j] = message[i]; // Put the value in a separate string
         j ++;
      }
-     result[j] = '\0';
+     result[j] = '\0'; // Add Null character
      Serial.println(message[1]);
-     if (message[3] == 'o') {
-        spe = atof(result);
-        if ((spe >= -0.75 && spe <= 0.75) || spe == -1.00) {
+     if (message[3] == 'o') { // Checking name of incoming data for Longitudinal Forces
+        spe = atof(result); // Transform the value to float
+        if ((spe >= -0.75 && spe <= 0.75) || spe == -1.00) { // Checking data to see if we should start the motor
             analogWrite(borneENA, 0);
-         } else if (spe > 0.75) {
+         } else if (spe > 0.75) { // Positive Longitudinal Forces
             digitalWrite(borneIN1, HIGH);
             digitalWrite(borneIN2, LOW);
-            analogWrite(borneENA, 250);
-         } else if (spe < -0.75) {
+            analogWrite(borneENA, 250); // Set direction of rotation and applying speed to the motor
+         } else if (spe < -0.75) { // Negative Longitudinal Forces
             digitalWrite(borneIN1, LOW);
             digitalWrite(borneIN2, HIGH);
-            analogWrite(borneENA, 250);
+            analogWrite(borneENA, 250);// Set direction of rotation and applying speed to the motor
          }
-     } else if (message[3] == 'a') {
-        mem = atof(result);
-        if ((mem >= -0.75 && mem <= 0.75) || mem == -1.00) {
+     } else if (message[3] == 'a') { // Checking name of incoming data for Lateral Forces
+        mem = atof(result); // Transform the value to float
+        if ((mem >= -0.75 && mem <= 0.75) || mem == -1.00) { 
             analogWrite(borneENB, 0);
-            //Serial.println("null");
-        } else if (mem > 0.75) {
+        } else if (mem > 0.75) { // Positive Lateral Forces
             digitalWrite(borneIN3, HIGH);
             digitalWrite(borneIN4, LOW);
-            analogWrite(borneENB, 250);
-            //Serial.println("plus");
-        } else if (mem < -0.75) {
+            analogWrite(borneENB, 250); // Set direction of rotation and applying speed to the motor
+        } else if (mem < -0.75) { // Negative Lateral Forces
             digitalWrite(borneIN3, LOW);
             digitalWrite(borneIN4, HIGH);
-            analogWrite(borneENB, 250);
+            analogWrite(borneENB, 250); // Set direction of rotation and applying speed to the motor
         }
      }
-      //je transforme la valeur de string a int ou float celon ce qui est recherché
-     //Serial.println(spe);
-     //spe = atof(result);
-     message[0] = "\0";
+     message[0] = "\0"; // reset data value 
      message_pos = 0;
-     // je reset les valeurs à défaut pour reboucler et recevoir de nouvelles valeurs
    }
  }
 }
